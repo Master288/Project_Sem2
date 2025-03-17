d398f1ca9fd74ee344b47f4444553dc11447db63
Master288 <2994656736@qq.com>
2025-03-17 21:35:39 +0800

    Jiaming Chen

---
+++ ex4.py
@@ -0,0 +1,14 @@
+#ex4
+
+x=int(input("enter your age:"))
+
+if x<20:
+    print("you have the qualify for student discounts");
+
+elif x>54:
+    print("you have the qualify for senior discounts");
+
+else:
+    print("you have no age discount");
+
+ 
